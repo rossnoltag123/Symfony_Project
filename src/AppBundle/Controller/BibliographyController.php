1c3766d56817bf1db58f11638048f0502b1b1560
RossItb <b00083046@student.itb.ie>
2017-04-07 19:26:12 +0100

    New project

---
+++ src/AppBundle/Controller/BibliographyController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Bibliography;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+//@Route("/Bibliography")
+/**
+ * Class BibliographyController
+ * @package AppBundle\Controller
+ *
+ */
+class BibliographyController extends Controller
+{
+    /**
+     * @Route("/Bibliography/new")
+     */
+    public function newAction()//order of route...
+    {//adding to database
+        $bib = new Bibliography();
+        $bib->setName('Ross');
+        $bib->setDif('Test');
+
+        $entityManager = $this->getDoctrine()->getManager();//get manager
+        $entityManager->persist($bib);//tell to save
+        $entityManager->flush();// commit to the addition
+
+        return new Response('<html><body>Added Ross to database</body></html>');
+    }
+
+
+    /**
+     * @Route("/Bibliography/{wildcard}")
+     */
+    public function showAction($wildcard)
+    {
+        $notes = [
+          'New entry: Latest technology in 3d accelerated hardware',
+            'Old entry: A study on the lastest in game graphics',
+            'Admin entry: Update database'
+        ];
+
+        return $this->render('Bibliography/show.html.twig', [
+            'name'=>$wildcard,
+            'notes'=> $notes
+        ]);
+    }
+}
---
+++ src/AppBundle/Entity/Bibliography.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="bibliography")
+ */
+class Bibliography
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $dif;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDif()
+    {
+        return $this->dif;
+    }
+
+    /**
+     * @param mixed $ref
+     */
+    public function setDif($dif)
+    {
+        $this->dif = $dif;
+    }
+}
