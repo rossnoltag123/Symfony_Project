0c32af7362b8ee65f91d6909d5484ebba3b020f1
RossItb <b00083046@student.itb.ie>
2017-04-08 23:46:30 +0100

    few minor editions

--- src/AppBundle/Controller/BibliographyController.php
+++ src/AppBundle/Controller/BibliographyController.php
@@ -3,51 +3,98 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Bibliography;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-//@Route("/Bibliography")
+
 /**
  * Class BibliographyController
  * @package AppBundle\Controller
- *
+ * @Route("/")
  */
 class BibliographyController extends Controller
 {
+    /*-------------------------------------------------------------------------------------------------------------------*/
     /**
-     * @Route("/Bibliography/new")
+     * @Route("/Bibliography/" , name="homepage")
      */
-    public function newAction()//order of route...
+    public function indexAction(){
+        $nameIndexArray = [
+          'name' => 'Ross'
+        ];
+
+        $templateName = 'index';
+
+        return $this->render($templateName.'.html.twig',$nameIndexArray);
+    }
+
+    /*-------------------------------------------------------------------------------------------------------------------*/
+    /**
+     * @Route("/Bibliography/login" , name="login")
+     */
+    public function loginAction(){
+        $nameIndexArray = [
+            'name' => 'Login'
+        ];
+        $templateName = 'login';
+
+        return $this->render($templateName.'.html.twig',$nameIndexArray);
+    }
+
+    /*-------------------------------------------------------------------------------------------------------------------*/
+    /**
+     * @Route("/Bibliography/database_Add" )
+     */
+    public function databaseAction()//order of route...
     {//adding to database
+
         $bib = new Bibliography();
         $bib->setName('Ross');
         $bib->setDif('Test');
 
-        $entityManager = $this->getDoctrine()->getManager();//get manager
+        $entityManager = $this->getDoctrine()->getManager();//get  Entity manager
         $entityManager->persist($bib);//tell to save
         $entityManager->flush();// commit to the addition
-
 
         return new Response('<html><body>Added Ross to database</body></html>');
     }
 
+    /*-------------------------------------------------------------------------------------------------------------------*/
 
 
-    /**
-     * @Route("/Bibliography/{wildcard}")
-     */
-    public function showAction($wildcard)
-    {
-        $notes = [
-          'New entry: Latest technology in 3d accelerated hardware',
-            'Old entry: A study on the latest in game graphics',
-            'Admin entry: Update database'
-        ];
+//    /**
+//     * @Route("/Bibliography/{wildcard}")
+//     */
+//    public function showAction($wildcard)
+//    {
+//        return $this->render('Bibliography/login.html.twig', [
+//            'name'=>$wildcard
+//        ]);
+//    }
 
-        return $this->render('Bibliography/show.html.twig', [
-            'name'=>$wildcard,
-            'notes'=> $notes
-        ]);
-    }
+
+    /*-------------------------------------------------------------------------------------------------------------------*/
+    //JSON response + route generation---------Displaying database info
+//    /**
+//     * @Route("/Bibliography/{wildcard}/notes" , name="databaseInfo")
+//     * @Method("GET")
+//     */
+//    public function getJsonAction()
+//    {
+//        $notes = [
+//          ['id' =>1, 'username' => 'Ross', 'notes'=> ' Latest technology in 3d accelerated hardware'],
+//          ['id' =>2, 'username' => 'Ross', 'notes'=>   'A study on the latest in game graphics'],
+//            ['id' =>3, 'username' => 'Ross', 'notes'=>  'latest in game'],
+//        ];
+//
+//        $data = [
+//            'notes' => $notes
+//        ];
+//
+//        return new JsonResponse($data);
+//    }
 }
--- src/AppBundle/Entity/Bibliography.php
+++ src/AppBundle/Entity/Bibliography.php
@@ -75,4 +75,5 @@ class Bibliography
     {
         $this->dif = $dif;
     }
+
 }
